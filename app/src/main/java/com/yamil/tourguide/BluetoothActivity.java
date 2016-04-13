ad44a799fcfeaecd9ad3fc226807a369cac51080
yarrib76 <NetAcc10>
2016-04-13 10:44:51 -0300

    Cambios

--- app/src/main/java/com/yamil/tourguide/BluetoothActivity.java
+++ app/src/main/java/com/yamil/tourguide/BluetoothActivity.java
@@ -3,6 +3,7 @@ package com.yamil.tourguide;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -13,12 +14,17 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.yamil.tourguide.Data.Contract;
+import com.yamil.tourguide.Data.DbProvider;
+
 import java.util.ArrayList;
 
 public class BluetoothActivity extends AppCompatActivity {
     private Button btn_scan;
     private Button btn_btn_agrega_confirmados;
     private BluetoothAdapter mBluetoothAdapter;
+    private DbProvider provider;
+    private ContentValues contentValues;
     private ListView listaBlueTooth;
     private ArrayList<ListBlueTooth> datos;
     private AdapterBlueTooth adapterBlueTooth;
@@ -30,11 +36,14 @@ public class BluetoothActivity extends AppCompatActivity {
         btn_btn_agrega_confirmados = (Button) findViewById(R.id.btn_agrega_confirmados);
         listaBlueTooth = (ListView) findViewById(R.id.lista_blueTooth);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        provider = new DbProvider(this);
+        contentValues = new ContentValues();
         btn_scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 datos = new ArrayList<ListBlueTooth>();
                 adapterBlueTooth = new AdapterBlueTooth(BluetoothActivity.this, datos);
+                listaBlueTooth.setAdapter(adapterBlueTooth);
                 IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
                 filter.addAction(BluetoothDevice.ACTION_FOUND);
                 filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
@@ -59,7 +68,15 @@ public class BluetoothActivity extends AppCompatActivity {
         btn_btn_agrega_confirmados.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                provider.deleteAllTable(Contract.BluetoothActivos.TABLA_BLUETOOTHACTIVOS);
+                for (int i = 0; i < datos.size(); i++ ){
+                    if (datos.get(i).getActivo()){
+                        contentValues.put(Contract.BluetoothActivos.COL_NOMBRE,datos.get(i).getNombre());
+                        contentValues.put(Contract.BluetoothActivos.COL_MACADDRESS,datos.get(i).getMacAddress());
+                        contentValues.put(Contract.BluetoothActivos.COL_ESTADO,0);
+                        provider.insert(Contract.BluetoothActivos.TABLA_BLUETOOTHACTIVOS,contentValues);
+                    }
+                }
             }
         });
     }
